809600dab6f1dd063cedc13f99bbe20ce28970cf
teky6606 <snlglv1@gmail.com>
2025-06-01 09:48:37 +0700

    add  function savepost

--- Admin/asset/js/script.js
+++ Admin/asset/js/script.js
@@ -3,5 +3,41 @@ let editIndex = null;
 
 const postForm = document.getElementById('postForm');
 const title = document.getElementById('title');
+const imageinput = document.getElementById('imageinput');
 const content = document.getElementById('content');
 const postsContainer = document.getElementById('postsContainer');
+
+postForm.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+
+    const title2 = title.value.trim();
+    const content2 = content.value.trim();
+    const imageinput = imageinput.files[0];
+
+    if(imageinput)
+    {
+        const reader = new FileReader();
+        reader.onload = function (event)
+        {
+            const imgUrl = event.target.result;
+            savepost(title2,content2,imageinput);
+
+        };
+        reader.readAsDataURL(imageinput);
+    } 
+    else{
+        savepost(title2,content2,null);
+    }
+
+});
+
+// function savepost(title, content, imageURL)
+// {
+//     if(editIndex != null)
+//     {
+//         post[editIndex] = {
+//             title,content, imageinput :imageURL
+//         }
+//     }
+// }
